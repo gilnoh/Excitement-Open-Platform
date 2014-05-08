c740e0dabef141471a325e95cefce416f02cabec
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-05-08 19:41:45 +0200

    startedexample 1

---
+++ alignmentusageexample/src/main/java/eu/excitementproject/eop/example/example1.java
@@ -0,0 +1,21 @@
+package eu.excitementproject.eop.example;
+
+public class example1 {
+
+	/**
+	 * A simple example that shows some usage for the alignment.Target and alignment.Link. 
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		// - How can I put my annotations in Target? (some code)
+		// - How should I put my Link in CAS? (again some code)
+		// - How can I iterate all Links?
+		// - How can I check my Links are properly added to CAS, as expected by EDAs? (LinkProber, just as LAPCasProber)
+		// - How can I access my annotations back from FSArray[Annotation] within Target? (e.g. not just Annotation instance, but back to, say, Dependency, Token, etc)
+		// - How can I access who have annotated this?
+		// - Getting "unique" full ID for a Link instance? 
+
+	}
+
+}
