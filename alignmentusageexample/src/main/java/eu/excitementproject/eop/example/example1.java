413df7106fba28a753b3f6cb34e4452066ab25b9
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-05-09 13:49:44 +0200

    - minor comments added ... (work work)

--- alignmentusageexample/src/main/java/eu/excitementproject/eop/example/example1.java
+++ alignmentusageexample/src/main/java/eu/excitementproject/eop/example/example1.java
@@ -5,9 +5,19 @@ public class example1 {
 	/**
 	 * A simple example that shows some usage for the alignment.Target and alignment.Link. 
 	 * 
+	 * TODO: Some more comments? what's here, what will come later... ? 
+	 *  
+	 * The example assumes that you know the very basic of CAS. 
+	 * In case if you are not familiar with CAS, it would be better to first 
+	 * check the CAS usage example.  
+	 *
+	 * https://github.com/gilnoh/cas_access_example.git
+	 * (example-1 in the above git will introduce you to the basic usage of CAS.) 
+	 * 
 	 */
 	public static void main(String[] args) {
 		
+		// [ We have two types, for the alignment. Let's see some usage. ] 
 		// - How can I put my annotations in Target? (some code)
 		// - How should I put my Link in CAS? (again some code)
 		// - How can I iterate all Links?
