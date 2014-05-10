14ad506801045605db684207c5e52c2e28c30d56
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-05-10 23:22:06 +0200

    nearly finished ex1

--- alignmentusageexample/src/main/java/eu/excitementproject/eop/example/example1.java
+++ alignmentusageexample/src/main/java/eu/excitementproject/eop/example/example1.java
@@ -1,7 +1,6 @@
 package eu.excitementproject.eop.example;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.Iterator;
 
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
@@ -9,14 +8,10 @@ import org.apache.uima.jcas.cas.FSArray;
 import static eu.excitementproject.eop.lap.implbase.LAP_ImplBase.*; 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
+import eu.excitement.type.alignment.Link;
 import eu.excitement.type.alignment.Target;
 import eu.excitementproject.eop.lap.LAPAccess;
 import eu.excitementproject.eop.lap.dkpro.MaltParserEN;
-
-
-
-
-
 
 
 //import static org.apache.uima.fit.util.JCasUtil; 
@@ -26,15 +21,20 @@ public class example1 {
 
 	/**
 	 * A simple example that shows some usage for the alignment.Target and alignment.Link. 
-	 * 
-	 * TODO: Some more comments? what's here, what will come later... ? 
 	 *  
+	 * <P> 
 	 * The example assumes that you know the very basic of CAS. 
 	 * In case if you are not familiar with CAS, it would be better to first 
 	 * check the CAS usage example.  
-	 *
 	 * https://github.com/gilnoh/cas_access_example.git
 	 * (example-1 in the above git will introduce you to the basic usage of CAS.) 
+	 * 
+	 * <P> 
+	 * The code uses MaltParser LAP to get dependency annotations. The LAP uses 
+	 * TreeTagger modules --- thus you need to install them to run the code actually. 
+	 * 
+	 * <P> Example 2 will cover more; for example, Prober for link annotations within 
+	 * a CAS, and some utilities, etc. 
 	 * 
 	 */
 	public static void main(String[] args) throws Exception {		// (For the sake of simplicity, we will ignore all Exceptions. Just don't do that in your code...)  
@@ -42,15 +42,15 @@ public class example1 {
 		// We have two types, for the alignment. One is alignment.Target that holds
 		// various "annotation data". The other is alignment.Link, which actually connects
 		// two targets; one target in TextView, the other target in HypothesisView. 
-		// TODO put v3 slides link here. 
-		
 		// Let's see some usage for the types.
 		
+		// (You can find v3 link type definitions in the following URL.) 
+		// https://docs.google.com/presentation/d/1A06cK2XsnaU9QXa1Ku_diafjcWKsvMG-6N8G0KDZWZw/edit?usp=sharing		
+
 		// Before begin, let's have some CASes with some linguistic annotations. 
 		// MaltParserEN is a LAPAccess, that tokenizes, lemmatizes, and does dependency parsing		
 		LAPAccess lap = new MaltParserEN(); 
 		JCas JCas1 = lap.generateSingleTHPairCAS("I have a dog.", "I own a pet."); 
-//		JCas JCas2 = lap.generateSingleTHPairCAS("I have a dog.", "I own a pet."); 
 		
 		// Okay, now JCas1 and JCas2 have TextView:"I have a dog.", HypothesisView:"I own a pet." 
 		// And each view has annotations of "Token", "Lemma", and "Dependency". 
@@ -81,7 +81,7 @@ public class example1 {
 			// (Note that FSArray is not really a Java Array -- but FSArray) 
 			FSArray annots = new FSArray(textViewOfJCas1, 1); // this is a size 1 FSarray; if you put more, you need bigger array, of course... 
 			annots.set(0, token);
-			// 3) Okay, now the Target items are prepared. Put it. 
+			// 3) Okay, now the FSArray is prepared. Put it on field "targetAnnotations" 
 			tg.setTargetAnnotations(annots);
 			// 4) Set begin - end value of the Target annotation (just like any annotation)
 			// note that, setting of begin and end of Target is a convention. 
@@ -121,10 +121,14 @@ public class example1 {
 		
 		// Sure. Just as an example, putting a dependency triple (Token - Dependency - Token)
 		// as a Target
+		Target triple_on_T = null; 
 		{
-			Target triple_on_T = new Target(textViewOfJCas1); 
+			triple_on_T = new Target(textViewOfJCas1); 
 			FSArray fs_t = new FSArray(textViewOfJCas1, 3); 
-			Dependency dep = (JCasUtil.select(textViewOfJCas1, Dependency.class)).iterator().next(); 
+			Iterator<Dependency> itr = (JCasUtil.select(textViewOfJCas1, Dependency.class)).iterator(); 
+			Dependency dep = itr.next(); 
+			dep = itr.next(); // say, let's pick the 2nd dependency  "a --det--> dog"   
+
 			Token ta = dep.getDependent(); 
 			Token tb = dep.getGovernor(); 
 			// okay. we have (ta - dep - tb)
@@ -148,18 +152,155 @@ public class example1 {
 		// - Q3: Okay. now I see we can put any Annotation data added by LAP
 		//       in a Target.  
 		//       Can you show me how I can actually mark a alignment.Link? 
-		
+		{
+			// let's the Target-s from Q1. 
+			// text_targets[] = [ targets for "I", "have", "a", "dog", "." ] 
+			// hypo_targets[] = [ targets for "I", "own", "a", "pet", "." ] 		
+			
+			// Let's put two Links on JCas1 
+			// (dog - pet), and (have - own) 
+			
+			// Steps for making a link  
+			// 1) prepare the Link 
+			Link link1 = new Link(hypoViewOfJCas1); // note that, we will add the Link to HYPOTHESIS VIEW 
+			// 2) put the Targets 
+			// (here, we already have two Targets prepared for us in Q1) 
+			Target target_T = text_targets[1]; 
+			Target target_H = hypo_targets[1]; 
+			link1.setTSideTarget(target_T); 
+			link1.setHSideTarget(target_H); 
+			// 3) put the "three link information".
+			link1.setAlignerID("testAligner"); // ID of the alinger, or the resource behind the alinger  
+			link1.setVersion("1.0"); // version number of the aligner, or the resource 
+			link1.setInfo("local-entailment"); // detailed information about the relation. 
+			// 4) set begin-end 
+			// Note that, by convention, we use begin-end of Target of TSideTarget 
+			link1.setBegin(target_H.getBegin()); 
+			link1.setEnd(target_H.getBegin());
+			// 5) add to index 
+			link1.addToIndexes(); 
+			// Note that, by convention, we add Link to HSide (on HypothesisView) 
+			// (note that when we prepare the Link in 1), we used HYPOTHESIS view.) 
+						
+			// we also put second Link, just for next example. 
+			Link link2 = new Link(hypoViewOfJCas1); 
+			link2.setTSideTarget(text_targets[3]); 
+			link2.setHSideTarget(hypo_targets[3]); 
+			link2.setAlignerID("testAligner"); 
+			link2.setVersion("1.0"); 
+			link2.setInfo("local-entailment"); 
+			link2.setBegin(hypo_targets[3].getBegin()); 
+			link2.setEnd(hypo_targets[3].getEnd());
+			link2.addToIndexes(); 
+		}
+
 		// - Q4: Oja. How about the opposite? How can I get Links back from a 
 		//   CAS? "Give me all Links in a CAS!" 
-		
-		// - Q5: Say, I have a Link, with its two targets --- how can I get back 
-		//   the annotations that are linked by the Link? (in other words, how can I 
-		//   get back my annotations within a (or two) Target(s) 
-		
-		// - Q6: How can I access who have annotated this? 
 
-		// - Q7: I've heard that each Link has "unique" full-ID. How can I access them? 
+		Link myLink = null; 
+		{
+			// Getting Link back from a JCas is just like, getting back any 
+			// annotation type from a JCAS. 
+			// Just keep in mind that, it is the contract (or convention) 
+			// that all Links are annotated on HYPOTHESISVIEW.  
+			
+			// Let's start from JCas1 
+			// first get HypothesisView 
+			JCas hView = JCas1.getView(HYPOTHESISVIEW); 
+			
+			// get "Link" type annotations back.. 
+			for (Link l : JCasUtil.select(hView, Link.class))
+			{
+				System.out.println("Found a link!");
+				// you can access Link, as normal, annotation. Of course. 
+				System.out.println("Its TSideTarget covers " + l.getTSideTarget().getCoveredText()); 
+				System.out.println("Its HSideTarget covers " + l.getHSideTarget().getCoveredText()); 
+				
+				myLink = l; // saving for next Q  
+			}
+		}
+		
+		// - Q5: How can I get back the annotations (Token, Dependency, NER, etc) 
+		//   that are stored in the Target? 
+		//   Annotations stored in "Target" are casted back into "Annotation". 
+		//   You have to fetch the annotations back to what they are (e.g. Token, Dependency) 
+		//   Let's see how you do it. 
+		
+		{
+			Target aTarget = myLink.getTSideTarget(); 
+			// We already know that this aTarget has one Token in it. Let's fetch it.  
+			FSArray arr = aTarget.getTargetAnnotations(); 			
+			System.out.println("The last link, T side Target has Token "); 
+			for (Token t : JCasUtil.select(arr, Token.class))
+			{
+				System.out.println("Begin: " + t.getBegin()); 
+				System.out.println("End: " + t.getEnd());
+				System.out.println("Lemma: " + t.getLemma().getValue()); 
+				System.out.println("POS: " + t.getPos().getPosValue()); 
+			}
+			
+			arr = myLink.getHSideTarget().getTargetAnnotations(); 
 
+			System.out.println("The last link, H side Target has Token "); 
+			for (Token t : JCasUtil.select(arr, Token.class))
+			{
+				System.out.println("Begin: " + t.getBegin()); 
+				System.out.println("End: " + t.getEnd());
+				System.out.println("Lemma: " + t.getLemma().getValue()); 
+				System.out.println("POS: " + t.getPos().getPosValue()); 
+			}
+			
+			// And from Q2, we have a Target that holds Dependency and Token 
+			System.out.println("A target that hold dependency triple, it has; "); 
+			for (Dependency d: JCasUtil.select(triple_on_T.getTargetAnnotations(), Dependency.class))
+			{
+				System.out.println("Dependency");
+				System.out.println("Governer:" + d.getGovernor().getCoveredText()); 
+				System.out.println("Dependent:" + d.getDependent().getCoveredText()); 
+				System.out.println("Dependency:" + d.getDependencyType()); 
+			}
+			for (Token t: JCasUtil.select(triple_on_T.getTargetAnnotations(), Token.class))
+			{
+				System.out.println("Token");
+				System.out.println("Begin: " + t.getBegin()); 
+				System.out.println("End: " + t.getEnd());
+				System.out.println("Lemma: " + t.getLemma().getValue()); 
+				System.out.println("POS: " + t.getPos().getPosValue()); 				
+			}
+			
+			// Wait. Just in case I don't know what is being hold in 
+			// a Target's FSArray. Can I check its type, by running some code? --- 
+			// You can actually check the type (getType())  
+			// and even access all the data in it; as feature structures. 
+			// (rather a CAS way then JCas way) 
+			
+			// But its usage is rather limited (since you can't use any JCas 
+			// classes that wraps the UIMA types with such approach). 
+	
+		}
+		
+		// - Q6: How can I access who (which module) has annotated this Link? 
+		{
+			// access Link's alingerID.  
+			String alingerId = myLink.getAlignerID(); 
+			System.out.println(alingerId); 
+			
+			// This would declare which alinger (or its underlying resource) has 
+			// caused the Link into existance. 
+		}
+
+		// - Q7: I've heard that Link has "unique" full-ID. 
+		// How can I access them? 
+		{
+			// use Link's getID() to get full, (or long) ID. 
+			// String fullId = myLink.getID(); 
+			// note that this ID is actually a concatenation of 
+			// getAlingerID(), getAlingerVersion(), and getLinkInfo()
+			
+			// It is a convention that this long-ID will always identify 
+			// a Link-type instance as "unique". 
+			// (e.g. same long-ID means really the same relation 
+			// denoted by the same aligner.) 
+		}
 	}
-
 }
