7b297478c4e97e9620bc0102b5e45eca3ae10469
GilNoh <noh@cl.uni-heidelberg.de>
2014-05-10 15:27:14 +0200

    some update on example POM and example1

--- alignmentusageexample/src/main/java/eu/excitementproject/eop/example/example1.java
+++ alignmentusageexample/src/main/java/eu/excitementproject/eop/example/example1.java
@@ -1,5 +1,10 @@
 package eu.excitementproject.eop.example;
 
+import org.apache.uima.jcas.JCas;
+
+import eu.excitementproject.eop.lap.LAPAccess;
+import eu.excitementproject.eop.lap.dkpro.MaltParserEN;
+
 public class example1 {
 
 	/**
@@ -17,14 +22,53 @@ public class example1 {
 	 */
 	public static void main(String[] args) {
 		
-		// [ We have two types, for the alignment. Let's see some usage. ] 
-		// - How can I put my annotations in Target? (some code)
-		// - How should I put my Link in CAS? (again some code)
-		// - How can I iterate all Links?
-		// - How can I check my Links are properly added to CAS, as expected by EDAs? (LinkProber, just as LAPCasProber)
-		// - How can I access my annotations back from FSArray[Annotation] within Target? (e.g. not just Annotation instance, but back to, say, Dependency, Token, etc)
-		// - How can I access who have annotated this?
-		// - Getting "unique" full ID for a Link instance? 
+		// [ We have two types, for the alignment. One is alignment.Target that holds
+		// various "annotation data". The other is alignment.Link, which actually connects
+		// two targets; one target in TextView, the other target in HypothesisView. 
+		// TODO put v3 slides link here. 
+		
+		// Let's see some usage for the types.
+
+		// Before begin, let's have one CAS with some linguistic annotations. 
+		
+		LAPAccess lap = null; 
+		JCas JCas1 = null; 
+		try {
+			lap = new MaltParserEN(); 
+			JCas1 = lap.generateSingleTHPairCAS("I have a dog.", "I own a pet."); 
+		}
+		catch (Exception e)
+		{
+			System.out.println("Exception -- " + e.getMessage()); 
+			System.exit(1);
+		}
+		
+		// Okay, now JCas1 has TextView:"I have a dog.", HypothesisView:"I own a pet." 
+		// And each view has annotations of "Token", "Lemma", and "Dependency". 
+
+		
+		// - Q1: How can I put my annotations in Target? 
+
+		
+		// - Q2: Oh. I see that. But token seems... very simple. Can I also 
+		//   put something more complex? say, depenency-triple, or partial parse tree
+		//   in a Target instance? 
+		
+		
+		// - Q3: Okay. now I see we can put any Annotation data added by LAP
+		//       in a Target.  
+		//       Can you show me how I can actually mark a alignment.Link? 
+		
+		// - Q4: Oja. How about the opposite? How can I get Links back from a 
+		//   CAS? "Give me all Links in a CAS!" 
+		
+		// - Q5: Say, I have a Link, with its two targets --- how can I get back 
+		//   the annotations that are linked by the Link? (in other words, how can I 
+		//   get back my annotations within a (or two) Target(s) 
+		
+		// - Q6: How can I access who have annotated this? 
+
+		// - Q7: I've heard that each Link has "unique" full-ID. How can I access them? 
 
 	}
 
